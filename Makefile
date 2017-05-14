9bd5f9b3beef4d4d6ae85b225a9d3ba120056d1a
Javier Maldonado <3goliad@gmail.com>
2017-05-14 09:50:04 -0400

    Removed older project files, began new README

--- Makefile
+++
@@ -1,4 +0,0 @@
-all:
-	pandoc local.org -o README.md && rm local.org
-local:
-	pandoc README.md -o local.org
--- components/sh/src/parser.rs
+++
@@ -1,5 +0,0 @@
-use lexer;
-
-pub fn parse<C>(c: C) where C: Iterator<Item=char> {
-    unimplemented!();
-}
