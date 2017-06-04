1bb9224dd7dcd3a7c4bcfd0c443622ab9222fc75
3goliad <3goliad@gmail.com>
2017-06-04 12:45:44 +0000

    switched graphics backend so we could render text without massive pain

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,8 @@
 extern crate diesel;
 #[macro_use]
 extern crate diesel_codegen;
+extern crate piston_window;
 extern crate dotenv;
-extern crate glutin;
-extern crate libc;
-extern crate freetype;
-mod gl {
-  #![allow(non_upper_case_globals)]
-  include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
 mod schema;
 mod models;
 #[cfg(test)]
@@ -17,6 +11,7 @@ mod test;
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 use dotenv::dotenv;
+use piston_window::*;
 use std::env;
 fn main() {
   use schema::notes::dsl::*;
@@ -25,34 +20,27 @@ fn main() {
   let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
   let connection = SqliteConnection::establish(&db_url)
     .expect("failed to connect to database");
-  let ftlib = freetype::Library::init().unwrap();
-  let face = lib.new_face("./fonts/cmusr.ttf", 0).unwrap();
-  let face.set_char_size(40 * 64, 0, 50, 0).unwrap();
-  face.load_char('A').unwrap();
-  let glyph = face.glyph();
-  let events_loop = glutin::EventsLoop::new();
-  let window = glutin::Window::new(&events_loop);
-  unsafe { window.make_current() }.unwrap();
-  unsafe {
-    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
-    gl::ClearColor(0.0, 1.0, 0.0, 1.0);
-  }
-  loop {
-    let mut exited = false;
-    events_loop.poll_events(|event| match event {
-                              glutin::Event::WindowEvent {
-                                event: glutin::WindowEvent::Closed, ..
-                              } => {
-                                exited = true;
-                              },
-                              _ => (),
-                            });
-    if exited {
-      break;
-    }
-    unsafe {
-      gl::Clear(gl::COLOR_BUFFER_BIT);
-    }
-    window.swap_buffers().unwrap();
+  let mut window:PistonWindow = WindowSettings::new("Hello World!",
+                                                    [1280, 720])
+      .exit_on_esc(true)
+      .build()
+      .unwrap();
+  let factory = window.factory.clone();
+  let mut glyphs = Glyphs::new("./fonts/cmunrm.ttf", factory).unwrap();
+  window.set_lazy(true);
+  while let Some(event) = window.next() {
+    window.draw_2d(&event, |ctx, gfx| {
+      clear([1.0; 4], gfx);
+      rectangle([1.0, 0.0, 0.0, 1.0],
+                [0.0, 0.0, 100.0, 100.0],
+                ctx.transform,
+                gfx);
+      let text_transform = ctx.transform.trans(10.0, 100.0);
+      text::Text::new_color([0.0, 0.0, 0.0, 1.0], 32).draw("Hello World!",
+                                                           &mut glyphs,
+                                                           &ctx.draw_state,
+                                                           text_transform,
+                                                           gfx);
+    });
   }
 }
